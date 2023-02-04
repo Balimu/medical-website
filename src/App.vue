961903ed3497ca70b22bf03af9b8f91bd32e83ce
AmandineBI <amandine7moi92@hotmail.com>
2023-02-04 15:36:51 +0100

    Restructure project creating a view which contains components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeView />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeView
   }
 }
 </script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <NavBar />
+    <HelloWorld msg="Bienvenue sur le site médical Levecq-Maissin"/>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '@/components/HelloWorld.vue';
+import NavBar from '@/components/NavBar.vue';
+
+export default defineComponent({
+    name: 'HomeView',
+    
+    components: {
+        HelloWorld,
+        NavBar
+    }
+});
+</script>
