7c8a4f867564e15d415f24626c321233d8990fcc
AmandineBI <amandine7moi92@hotmail.com>
2023-02-04 20:30:36 +0100

    Add Vite, Tailwind, and router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1 class="text-3xl font-bold underline">This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,9 @@
-<template>
-    <NavBar />
-    <HelloWorld msg="Bienvenue sur le site médical Levecq-Maissin"/>
-</template>
-
-<script>
-import { defineComponent } from 'vue';
-
-// Components
-import HelloWorld from '@/components/HelloWorld.vue';
-import NavBar from '@/components/NavBar.vue';
-
-export default defineComponent({
-    name: 'HomeView',
-    
-    components: {
-        HelloWorld,
-        NavBar
-    }
-});
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
 </script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
