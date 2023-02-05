a2bf61ee1449908666fe9203531c91bacbad1607
Lucia L <lucia@MacBook-Air-de-Lucia.local>
2023-02-05 20:05:20 +0100

    Link added mobminder

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -34,7 +34,7 @@ export default {
 
 <template>
 
-    <div class="items-center mx-auto w-full max-w-screen-2xl px-4 grid grid-cols-[2fr_1fr]">
+    <div class="mx-auto w-full max-w-screen-2xl px-4 grid grid-cols-[2fr_1fr]">
 
         <div class="col1 px-8">
             <div class="mt-12 text-2xl text-left h-12 md:text-4xl">
@@ -52,7 +52,11 @@ export default {
         </div>
 
         <div class="col2 mt-12 pt-12 px-8 mr-8">
-            <button class="bg-[#4a9dd1] text-white font-bold py-2 px-4 rounded">Prendre rendez-vous avec la Docteure Levecq</button>
+            <a href="https://be.mobminder.com/e-resa.php?p=drcelinelevecq">
+                <button class="bg-[#4a9dd1] text-white font-bold py-2 px-4 rounded">
+                Prendre rendez-vous avec la Docteure Levecq
+                </button>
+            </a>
             <button type="button" @mouseover="hover = true" @mouseleave="hover = false" class="relative bg-[#4a9dd1] text-white py-2 px-4 rounded mt-4">
                 <div class="font-bold invisible">Prendre rendez-vous avec la Docteure Maissin</div>
                 <div :class="{ invisible: hover }" class="font-bold button-label">Prendre rendez-vous avec la Docteure Maissin</div>
