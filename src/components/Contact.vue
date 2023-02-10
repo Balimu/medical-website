548b965e06a5eac57bfc96dee76febc948b53b73
AmandineBI <amandine7moi92@hotmail.com>
2023-02-10 23:59:28 +0100

    Button as components and add them to Contact section

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,21 +1,12 @@
-<template>
-    <div class="mt-20 mb-16 w-full text-right">
-
-        <iframe
-            width="600"
-            height="450"
-            style="border:0"
-            loading="lazy"
-            allowfullscreen
-            referrerpolicy="no-referrer-when-downgrade"
-            :src="'https://www.google.com/maps/embed/v1/place?key=' + mapsApiKey + '&q=docteur+céline+levecq,Mons'"
-        >
-        </iframe>
-    </div>
-</template>
-
 <script>
+import ButtonMaissin from './buttons/ButtonMaissin.vue';
+import ButtonLevecq from './buttons/ButtonLevecq.vue';
+
 export default {
+    components: {
+        ButtonLevecq,
+        ButtonMaissin,
+    },
     data: () => {
         return {
             mapsApiKey: import.meta.env.VITE_MAPS_API_KEY,
@@ -23,3 +14,30 @@ export default {
     }
 }
 </script>
+
+<template>
+    <div class="mx-8 text-left">
+        <div id="contact" class="h-16"></div>
+        <h1 class="text-3xl font-bold mt-4">
+            Contact
+        </h1>
+        
+        <div class="mt-16 mb-32 grid grid-cols-2">
+            <div class="col1 ml-8">
+                <p class="mb-4">
+                    <span class="font-bold">Adresse: </span><br/> Rue de Bouzanton 11, <br/>
+                    7000 Mons, Belgique
+                </p>
+                <ButtonLevecq />
+                <ButtonMaissin />
+            </div>
+            <div class="col2">
+                <iframe width="400" height="300" loading="lazy" allowfullscreen referrerpolicy="no-referrer-when-downgrade"
+                    :src="'https://www.google.com/maps/embed/v1/place?key=' + mapsApiKey + '&q=docteur+céline+levecq,Mons'"
+                ></iframe>
+            </div>
+
+        </div>
+
+    </div>
+</template>
