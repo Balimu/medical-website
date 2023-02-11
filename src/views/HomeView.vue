1b746655864fb1f23a50234ca576ba73a09eef70
AmandineBI <amandine7moi92@hotmail.com>
2023-02-11 22:55:11 +0100

    Small changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script>
-import NavBar from "../components/NavBar.vue";
-import Accueil from "../components/Accueil.vue";
-import Footer from "../components/Footer.vue";
-import NosMedecins from "../components/NosMedecins.vue";
-import Contact from "../components/Contact.vue";
+import NavBar from "@/components/NavBar.vue";
+import Accueil from "@/components/Accueil.vue";
+import Footer from "@/components/Footer.vue";
+import NosMedecins from "@/components/NosMedecins.vue";
+import Contact from "@/components/Contact.vue";
 
 export default {
   components: {
